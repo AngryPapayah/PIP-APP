3888ae4075f3ffa19d5894c59cb381cd1e226723
eline <1096534@hr.nl>
2026-06-03 11:02:42 +0200

    ADD variants, styling, sizes to button component

--- src/components/CustomButton.js
+++ src/components/CustomButton.js
@@ -1,7 +1,79 @@
-import {Text} from 'react-native';
+import {Text, StyleSheet, Pressable, View} from 'react-native';
+import {colors, globalStyles} from "../styles/GlobalStyles.js";
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 
-export const CustomButton = () => {
+
+export const CustomButton = ({variant, size, onPress, children, disabled}) => {
+
+    const isRightAnswer = variant === 'rightAnswer';
+    const isWrongAnswer = variant === 'wrongAnswer';
+
     return (
-        <Text>Button</Text>
+        <Pressable onPress={onPress} style={[
+            styles.ButtonContainer,
+            sizes[size],
+            variants[variant],
+            disabled && {opacity: 0.3}
+        ]}>
+            <Text style={styles.ButtonText}>{children}</Text>
+
+            {isRightAnswer && (
+                <View style={styles.IconContainer}>
+                    <FontAwesome name="thumbs-up" size={18} color={'#047053'}></FontAwesome>
+                </View>
+            )}
+
+            {isWrongAnswer && (
+                <View style={styles.IconContainer}>
+                    <FontAwesome name="thumbs-down" size={18} color={'#802636'}></FontAwesome>
+                </View>
+            )}
+        </Pressable>
     )
 }
+
+const variants = {
+    primary: {
+        backgroundColor: colors.primaryButton,
+        borderWidth: 1,
+        borderRadius: 10,
+        borderColor: colors.accent
+    },
+    rightAnswer: {
+        backgroundColor: colors.rightButton,
+        borderWidth: 1,
+        borderRadius: 10,
+        borderColor: colors.accent,
+        position: 'relative',
+        paddingBottom: 20,
+
+    },
+    wrongAnswer: {
+        backgroundColor: colors.wrongButton,
+        borderWidth: 1,
+        borderRadius: 10,
+        borderColor: colors.accent
+    },
+    icon: {backgroundColor: 'transparent'},
+}
+
+const sizes = {
+    sm: {paddingVertical: 6, paddingHorizontal: 16},
+    md: {paddingVertical: 10, paddingHorizontal: 32},
+    lg: {paddingVertical: 14, paddingHorizontal: 45},
+};
+
+const styles = StyleSheet.create({
+    ButtonContainer: {
+        margin: 10,
+    },
+    ButtonText: {
+        fontSize: globalStyles.text.fontSize,
+        textAlign: 'center',
+    },
+    IconContainer: {
+        position: 'absolute',
+        bottom: 8,
+        right: 12,
+    }
+})
