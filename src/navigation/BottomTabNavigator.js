855872700dede67734df5660cef7e5cec5f39e57
AngryPapayah <airissavermo@hotmail.com>
2026-06-03 09:53:52 +0200

    Screens added: HamsterverseScreen.js, ProfileScreen.js

--- src/navigation/BottomTabNavigator.js
+++ src/navigation/BottomTabNavigator.js
@@ -3,6 +3,8 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import HomeScreen from '../screens/HomeScreen';
 import SettingsScreen from '../screens/SettingsScreen';
 import {colors} from '../styles/GlobalStyles';
+import ProfileScreen from "../screens/ProfileScreen";
+import HamsterverseScreen from "../screens/HamsterverseScreen";
 
 const Tab = createBottomTabNavigator();
 
@@ -21,8 +23,10 @@ export default function BottomTabNavigator() {
                 headerTintColor: colors.textCard,
             }}
         >
+            <Tab.Screen name="Hamsterverse" component={HamsterverseScreen}/>
             <Tab.Screen name="Home" component={HomeScreen}/>
-            {/*<Tab.Screen name="Settings" component={SettingsScreen} />*/}
+            <Tab.Screen name="Profile" component={ProfileScreen}/>
+            {/*<Tab.Screen name="Profile" component={ProfileScreen}/>*/}
         </Tab.Navigator>
     );
 }
