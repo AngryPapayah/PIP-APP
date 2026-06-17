b737c42e355c975ebd0585bed06e7c1c19804a6a
AngryPapayah <airissavermo@hotmail.com>
2026-06-17 15:17:23 +0200

    safeareaview

--- src/components/MultipleChoice.js
+++ src/components/MultipleChoice.js
@@ -3,6 +3,7 @@ import {ScrollView, StyleSheet, View, Image} from "react-native";
 import {colors} from "../styles/GlobalStyles";
 import React, {useEffect, useState} from "react";
 import TextBubble from "./TextBubble";
+import {SafeAreaView} from "react-native-safe-area-context";
 
 export default function MultipleChoice({question, onNext, isLastQuestion}) {
     const [selectedAnswerId, setSelectedAnswerId] = useState(null);
@@ -39,59 +40,70 @@ export default function MultipleChoice({question, onNext, isLastQuestion}) {
     }
 
     return (
-        <ScrollView style={styles.scrollView} contentContainerStyle={styles.scrollContainer}>
-            {hasAnswered ? (
-                <View style={styles.container}>
-                    {question?.explanation && (
-                        <TextBubble text={question?.explanation}></TextBubble>
-                    )}
-                    <Image source={pipImage} style={styles.imageStyle} resizeMode="contain" />
-                    <View>
-                        <CustomButton
-                            variant="primary"
-                            size="sm"
-                            onPress={() => onNext(isCorrectAnswer, selectedAnswerId)}>
-                            {isLastQuestion ? "Finish" : "Next question >>>"}
-                        </CustomButton>
+        <SafeAreaView style={styles.safeAreaView}>
+
+            <ScrollView contentContainerStyle={styles.container} onLayout={() => setIsLayoutReady(true)}>
+
+                {hasAnswered ? (
+                    <View style={styles.container}>
+                        {question?.explanation && (
+                            <TextBubble text={question?.explanation}></TextBubble>
+                        )}
+                        <Image source={pipImage} style={styles.imageStyle} resizeMode="contain"/>
+                        <View>
+                            <CustomButton
+                                variant="primary"
+                                size="sm"
+                                onPress={() => onNext(isCorrectAnswer, selectedAnswerId)}>
+                                {isLastQuestion ? "Finish" : "Next question >>>"}
+                            </CustomButton>
+                        </View>
                     </View>
-                </View>
-            ) : (
-                <View style={styles.container}>
-                    <TextBubble text={question?.question_text || "Loading Question..."}></TextBubble>
-                    <Image source={pipImage} style={styles.imageStyle} resizeMode="contain" />
-                </View>
-            )}
-            <View style={styles.answersContainer}>
-                {question?.answers?.map((answer) => {
-                    let currentVariant = "questionButton";
-                    if (hasAnswered) {
-                        if (answer.is_correct === 1 || answer.is_correct === true) {
-                            currentVariant = "rightAnswer";
-                        } else if (answer.id === selectedAnswerId) {
-                            currentVariant = "wrongAnswer";
+                ) : (
+                    <View style={styles.container}>
+                        <TextBubble text={question?.question_text || "Loading Question..."}></TextBubble>
+                        <Image source={pipImage} style={styles.imageStyle} resizeMode="contain"/>
+                    </View>
+                )}
+                <View style={styles.answersContainer}>
+                    {question?.answers?.map((answer) => {
+                        let currentVariant = "questionButton";
+                        if (hasAnswered) {
+                            if (answer.is_correct === 1 || answer.is_correct === true) {
+                                currentVariant = "rightAnswer";
+                            } else if (answer.id === selectedAnswerId) {
+                                currentVariant = "wrongAnswer";
+                            }
                         }
-                    }
-                    return (
-                        <CustomButton
-                            key={answer.id}
-                            variant={currentVariant}
-                            size="sm"
-                            onPress={() => handleAnswer(answer)}
-                            disabled={hasAnswered}
-                        >{answer.answer_text}</CustomButton>
-                    );
-                })}
-            </View>
-        </ScrollView>
+                        return (
+                            <CustomButton
+                                key={answer.id}
+                                variant={currentVariant}
+                                size="sm"
+                                onPress={() => handleAnswer(answer)}
+                                disabled={hasAnswered}
+                            >{answer.answer_text}</CustomButton>
+                        );
+                    })}
+                </View>
+            </ScrollView>
+        </SafeAreaView>
     );
 }
 
 const styles = StyleSheet.create({
-    scrollView: {
+    safeAreaView: {
         flex: 1,
+        backgroundColor: colors?.primary || '#F4E1C1',
     },
-    container: { padding: 20, backgroundColor: colors?.primary || '#FFDFAD', alignItems: 'center' },
-    scrollContainer: { padding: 5, backgroundColor: colors?.primary || '#FFDFAD', alignItems: 'center', paddingBottom: 50, flexGrow: 1 },
-    answersContainer: { width: "100%" },
-    imageStyle: { width: 200, height: 200, marginLeft: 15 }
+    container: {padding: 20, backgroundColor: colors?.primary || '#FFDFAD', alignItems: 'center'},
+    scrollContainer: {
+        padding: 5,
+        backgroundColor: colors?.primary || '#FFDFAD',
+        alignItems: 'center',
+        paddingBottom: 50,
+        flexGrow: 1
+    },
+    answersContainer: {width: "100%"},
+    imageStyle: {width: 200, height: 200, marginLeft: 15}
 });
